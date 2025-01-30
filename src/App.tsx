16606aed038011a7f1e56146d2b63bea81cf8093
Nicolás Rodrígues Chaves <nicolasrodriguezch@hotmail.com>
2025-01-30 10:27:33 -0500

    install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <>
-     <h1>Yo whats gucci</h1>
+      <h1>Hello world!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
